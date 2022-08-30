32d11f635409a7c158f690a167e09f520cd038e7
TigranMikayelyan <112478852+TigranMikayelyan@users.noreply.github.com>
2022-08-30 14:18:15 +0400

    Add files via upload

---
+++ Python/2.py
@@ -0,0 +1,13 @@
+# 2. Մուտքագրել երեք փոփոխականներ, տպել նվազագույնի արժեքը։ 
+
+a = int(input("type first num "))
+b = int(input("type second num "))
+c = int(input("type third num "))
+if a < b and a < c :
+        print (a)
+
+elif b < a and b < c :
+        print (b)
+
+else:
+        print(c)
---
+++ Python/7.py
@@ -0,0 +1,8 @@
+# 7. Հայտարարել և մուտքագրել երկու փոփոխականներ։ Փոխել փոփոխականների արժեքները տեղերով և տպել էկրանին։
+
+x = int (input("type first num  "))
+y = int (input("type second num "))
+temp = x
+x = y
+y = temp
+print (x,y)
