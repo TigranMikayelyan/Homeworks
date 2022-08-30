48f55bf3ededed5b79aede5107ebbae0b7bd28cf
TigranMikayelyan <112478852+TigranMikayelyan@users.noreply.github.com>
2022-08-30 14:19:01 +0400

    Create 1.js

---
+++ Javascript/1.js
@@ -0,0 +1,17 @@
+// 1. Մուտքագրել երեք փոփոխականներ, տպել առավելագույնի արժեքը։
+
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+if (num1 > num2 && num1 > num3)
+{
+	console.log(num1);
+}
+else if (num2 > num1 && num2 > num3)
+{
+	console.log(num2);
+}
+else
+{
+	console.log(num3);
+}
